b5f6bdc65b6b2d557e632b29c5e41a2766656e33
ganesh <ganeshgajula22@gmail.com>
2021-01-14 15:31:29 +0530

    feat: searching stockname from searchBar

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const searchBar = document.querySelector("#searchBar");
+// const searchBar = document.querySelector("#searchBar");
 // const submitBtn = document.querySelector("#searchBtn");
 
 const serverUrl =
@@ -11,13 +11,62 @@ function constructUrl(stockName) {
   // console.log(finalUrl);
 }
 
-function clickHandler() {
-  let stockName = searchBar.value;
-  console.log(stockName);
+// function clickHandler() {
+//   let stockName = searchBar.value;
+//   console.log(stockName);
 
-  fetch(constructUrl(stockName))
-    .then((response) => response.json())
-    .then((json) => console.log(json.bestMatches[0].symbol));
-}
+//   fetch(constructUrl(stockName))
+//     .then((response) => response.json())
+//     .then((json) => console.log(json.bestMatches[1]["1. symbol"]));
+// }
 
-searchBar.addEventListener("keyup", clickHandler);
+// searchBar.addEventListener("keyup", clickHandler);
+
+const stocksList = document.querySelector("#stocksList");
+const searchBar = document.querySelector("#searchBar");
+let hpCharacters = [];
+
+searchBar.addEventListener("keyup", (event) => {
+  const searchString = event.target.value;
+  // const filteredCharacters = arrayHpCharacters.filter((character) => {
+  //   return (
+  //     character.name.toLowerCase().includes(searchString) ||
+  //     character.house.toLowerCase().includes(searchString)
+  //   );
+  // });
+  // displayCharacters(filteredCharacters);
+  loadCharacters(searchString);
+});
+
+const loadCharacters = async (searchString) => {
+  try {
+    // console.log(stockName);
+    const response = await fetch(constructUrl(searchString));
+    hpCharacters = await response.json();
+    console.log(hpCharacters);
+    let arrayHpCharacters = hpCharacters.bestMatches;
+    displayCharacters(arrayHpCharacters);
+    // console.log(hpCharacters);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const displayCharacters = (characters) => {
+  console.log(characters);
+  const htmlString = characters
+    .map((character) => {
+      return `
+    <li class = "character">
+      <h2>${character["1. symbol"]}</h2>
+      <p>House: ${character["2. name"]}</p>
+      <h3> Region: ${character["4. region"]}
+
+    `;
+    })
+    .join("");
+
+  stocksList.innerHTML = htmlString;
+};
+
+loadCharacters();
