d6d6b7ffa3ec4c1171b5801f3571cf26179463a3
ganesh <ganeshgajula22@gmail.com>
2021-01-14 09:02:00 +0530

    feat: fetching data from api

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// Api key :  MGA2XVI896C34RWQ
+const searchBar = document.querySelector("#searchBar");
+// const submitBtn = document.querySelector("#searchBtn");
+
+const serverUrl =
+  "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=";
+
+const apiKey = "MGA2XVI896C34RWQ";
+
+function constructUrl(stockName) {
+  return serverUrl + stockName + "&" + "apikey=" + apiKey;
+  // console.log(finalUrl);
+}
+
+function clickHandler() {
+  let stockName = searchBar.value;
+  console.log(stockName);
+
+  fetch(constructUrl(stockName))
+    .then((response) => response.json())
+    .then((json) => console.log(json.bestMatches[0].symbol));
+}
+
+searchBar.addEventListener("keyup", clickHandler);
